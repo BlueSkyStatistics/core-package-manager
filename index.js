033836f79ee27cab9d597f37e963d5bfd7a24b1e
Aspect13 <ad13box@gmail.com>
2023-02-22 20:12:20 +0400

    DialogsPackage done

--- index.js
+++ index.js
@@ -2,3 +2,15 @@
 //     BSEvent,
 //     css: ['notifyer.css']
 // }
+
+const PackageManager = require("./js/PackageManager")
+const LocalPackage = require("./js/LocalPackage")
+const RemotePackage = require("./js/RemotePackage")
+const DialogsPackage = require("./js/DialogsPackage")
+
+module.exports = {
+    PackageManager,
+    LocalPackage,
+    RemotePackage,
+    DialogsPackage
+}
---
+++ js/DialogsPackage.js
@@ -0,0 +1,63 @@
+const LocalPackage = require("./localPackage")
+
+
+class DialogsPackage extends LocalPackage {
+    constructor(packageJson) {
+        super(packageJson)
+        this.nav = this._getNav()
+    }
+
+    _getNav() {
+        let importPath = this.realImportPath
+        let packageNav
+        try {
+            packageNav = require(importPath).nav
+            console.log(`Importing from ${importPath}`)
+        } catch(ex) {
+            importPath = this.devImportPath
+            packageNav = require(importPath).nav
+            console.log(`Importing from ${importPath}`)
+        }
+        const pathAddon = importPath.replace("nav.js", "")
+
+        let packageNavList = []
+        if (packageNav.buttons !== undefined) {
+            packageNavList.push(packageNav)
+        } else {
+            packageNavList = packageNav
+        }
+        // var navList = []
+        const navList = packageNavList.map(p => {
+            p.buttons = p.buttons.map(b => {
+                if (typeof b === 'object') {
+                    if (b.children === undefined) {
+                        console.log("We should not be here, unless we trying to store some object in the nav")
+                    } else {
+                        b.children = b.children.map(c => path.join(pathAddon, c))
+                    }
+                } else {
+                    return path.join(pathAddon, b)
+                }
+            })
+            delete require.cache[importPath]
+            return p
+
+            // for(const level = 0; level < p.buttons.length; level++) {
+            //     if (typeof(p.buttons[level]) == "object" && p.buttons[level].children == undefined) {
+            //         console.log("We should not be here, unless we trying to store some object in the nav")
+            //     } else if (typeof(p.buttons[level]) == "object" && p.buttons[level].children != undefined) {
+            //         for (var sublevel = 0; sublevel < p.buttons[level].children.length; sublevel++) {
+            //             p.buttons[level].children[sublevel] = path.join(pathAddon, p.buttons[level].children[sublevel])
+            //         }
+            //     } else {
+            //         p.buttons[level] = path.join(pathAddon, p.buttons[level])
+            //     }
+            // }
+            // delete require.cache[importPath]
+            // navList.push(packagenav)
+        })
+        return navList
+    }
+}
+
+module.exports = DialogsPackage

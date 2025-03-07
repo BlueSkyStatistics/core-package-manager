a2c5d77f3cf8229916ee885587508324483e7fbf
adpgithub <anildabralpro@gmail.com>
2025-03-07 17:20:20 +0530

    copyright added

--- js/RemotePackage.js
+++ js/RemotePackage.js
@@ -1,3 +1,9 @@
+/**
+  * This file is protected by copyright (c) 2023-2025 by BlueSky Statistics, LLC.
+  * All rights reserved. The copy, modification, or distribution of this file is not
+  * allowed without the prior written permission from BlueSky Statistics, LLC.
+ */
+
 const axios = require('axios')
 const {maxSatisfying: semverMaxSatisfying, lte: semverLte, satisfies: satisfyVer} = require('semver')
 const {Render} = require('squirrelly')

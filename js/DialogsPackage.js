c6559d1083b87d51d826e47dfad9ac2398d7b5cc
adpgithub <anildabralpro@gmail.com>
2025-09-08 14:23:00 +0530

    An issue where, if you do not use utility.asar and rather load it from the source all dialog's path appears duplicated (in appconfig in APPDATA) for each dialog and the app UI does not load correctly. First launch usually does not show this issue but the paths are already incorrect. On the second launch the paths do not work and the UI does not show up correctly. UI is like squeezed on the left of the screen. Third launch and after that usually the issue is gone. If you clear appdata then same thing happens again. In this fix we are checking if the dailog path is already correct then do not try to append more to it.  Seems to work well. This was needed for no-asar build.

--- js/DialogsPackage.js
+++ js/DialogsPackage.js
@@ -4,10 +4,10 @@
   * allowed without the prior written permission from BlueSky Statistics, LLC.
  */
 
-const {join, normalize} = require('path')
+const {join, normalize, dirname} = require('path')
 
 try {
-    var LocalPackage = require("./localPackage")
+    var LocalPackage = require("./LocalPackage")
 } catch (ex) {
     var LocalPackage = require(path.normalize(__dirname + "/LocalPackage"));
 }
@@ -35,14 +35,16 @@ class DialogsPackage extends LocalPackage {
             console.warn(`Could not import ${importPath}`)
             return []
         }
-        const pathAddon = importPath.replace("nav.js", "")
-
+        const pathAddon = normalize(importPath.replace("nav.js", ""))
+        const modalsPath = normalize(dirname(pathAddon));
+        ipcRenderer.invoke("log", { message: `getnav addonpath: ${pathAddon}` , source: "_DP", event: "spawn" })
         let packageNavList = []
         if (packageNav.buttons !== undefined) {
             packageNavList.push(packageNav)
         } else {
             packageNavList = packageNav
         }
+        // store.set("ResumeStatus", packageNavList)
         // var navList = []
         const navList = packageNavList.map(p => {
             p.buttons = p.buttons.map(b => {
@@ -50,11 +52,26 @@ class DialogsPackage extends LocalPackage {
                     if (b.children === undefined) {
                         ipcRenderer.invoke("log", { message: `We should not be here, unless we trying to store some object in the nav` , source: "DialogsPackage", event: "spawn" })
                     } else {
-                        b.children = b.children.map(c => normalize(join(pathAddon, c)))
+                        // b.children = b.children.map(c => normalize(join(pathAddon, c)))// this line duplication issue. Replaced with following
+                        
+                        b.children = b.children.map(c => {
+                            //ipcRenderer.invoke("log", { message: `Processing nav menu dialog ${c}` , source: "DialogsPackage", event: "spawn" })
+                            if (normalize(c).startsWith(modalsPath)) {
+                                return normalize(c)
+                            } else {
+                                return normalize(join(pathAddon, c))
+                            }  
+                        })
                     }
                     return b
                 } else {
-                    return normalize(join(pathAddon, b))
+                    // return normalize(join(pathAddon, b))// this line duplication issue. Replaced with following
+                    //ipcRenderer.invoke("log", { message: `Processing nav leaf dialog ${b}` , source: "DialogsPackage", event: "spawn" })
+                    if (normalize(b).startsWith(modalsPath)) {
+                        return normalize(b);
+                    } else {
+                        return normalize(join(pathAddon, b));
+                    }
                 }
             }).filter(b => b !== null)
             delete require.cache[importPath]
